977130ebc518aade21066516a24ed78f86622de2
davidfunderburk <dafun2@morgan.edu>
2019-08-05 09:43:01 -0400

    New changes

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,14 @@
 package hbcu.stay.ready.algor;
 
-public class Solution {
-
-    public String countUniqueWords(String input){
-        return null;
-    }
+//public void setup(){solution = new Solution();}
+public static void countUniqueWords(){
+    String s = "Hello Java. Hello World. Hello Java!";
+    List<String> expected = new ArrayList<>();
+    List<Integer> expected1 = new ArrayList<>();
+    countWords(s, output, count);
+    System.out.println(expected);
+    System.out.println(expected1);
 }
+
+
+//  The code is not general, It is supposed to only work for the 1st example.
