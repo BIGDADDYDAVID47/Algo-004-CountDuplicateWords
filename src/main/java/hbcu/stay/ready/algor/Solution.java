15af7efef1b254244539c825eee2b7091e490870
davidfunderburk <dafun2@morgan.edu>
2019-08-05 22:39:37 -0400

    new changes

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,14 +1,42 @@
-package hbcu.stay.ready.algor;
+//package hbcu.stay.ready.algor;
 
-//public void setup(){solution = new Solution();}
-public static void countUniqueWords(){
-    String s = "Hello Java. Hello World. Hello Java!";
-    List<String> expected = new ArrayList<>();
-    List<Integer> expected1 = new ArrayList<>();
-    countWords(s, output, count);
-    System.out.println(expected);
-    System.out.println(expected1);
-}
+//public class Solution {
 
-
+    //public String countUniqueWords(String input){
+        //return null;
+ //   }
+//}
 //  The code is not general, It is supposed to only work for the 1st example.
+
+public class Solution
+
+{
+public String countUniqueWords(String input){
+    return input;
+}
+    public static void main(String[] args)
+    {
+        String input="Hello Java. Hello World. Hello Java!";
+        String[] words=input.split(" ");
+        int wrc=1;
+
+        for(int i=0;i<words.length;i++)
+        {
+            for(int j=i+1;j<words.length;j++)
+            {
+
+                if(words[i].equals(words[j]))
+                {
+                    wrc=wrc+1;
+                    words[j]="0";
+                }
+            }
+            if(words[i]!="0")
+                System.out.println(words[i]+"--"+wrc);
+            wrc=1;
+
+        }
+
+    }
+
+}
